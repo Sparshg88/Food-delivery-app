d703ce8f63effc9ca0a3e1572210ebdcaabcf0bf
Sparshg88 <131021082+Sparshg88@users.noreply.github.com>
2026-02-04 16:50:32 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://sparshgupta:sparsh123@cluster0.ybm8jrz.mongodb.net/food-del').then(()=> console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://user:password@cluster0.mongodb.net/food-del').then(()=> console.log("DB Connected"));
 }
